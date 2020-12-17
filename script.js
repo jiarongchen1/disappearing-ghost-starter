02046431f0bbbe8dd825f29035dbc082cac87468
Glitch (jiarongchen1-disappearing-ghost-starter) <none>
2020-12-17 00:48:54 +0000

    🏄🏣 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+$(".hide-button").click(function(){
+    $("img").hide();
+});
 
+$(".show-button").click(function(){
+    $("img").show();
+});
+
+$(".update-img-button").click(function(){
+$("img").attr("src", "https://illustoon.com/photo/3487.png");
+});
+
+$(".message-button").click(function(){
+let message = $("input").val();
+   $(".message").append(message);
+});
+
+$(".name-button").click(function(){
+let message = $("input").val();
+   $("h1").text(message);
+});
